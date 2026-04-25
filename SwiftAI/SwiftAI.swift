55f3e6aa082df173949233db80c1565a97b23d7e
Adarsh Pandey <adarsh.official2299@gmail.com>
2026-04-25 23:03:53 +0530

    Initial commit

---
+++ SwiftAI/SwiftAI.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftAI.swift
+//  SwiftAI
+//
+//  Created by Adarsh on 25/04/26.
+//
+
+import Foundation
+
---
+++ SwiftAITests/SwiftAITests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAITests.swift
+//  SwiftAITests
+//
+//  Created by Adarsh on 25/04/26.
+//
+
+import Testing
+@testable import SwiftAI
+
+struct SwiftAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
